963cc5fb608f5eefe97edda870bfa0ae338ff7af
Shajeed <shajeed0@gmail.com>
2020-12-05 22:07:55 -0500

    updated team paGE

---
+++ direction.php
@@ -0,0 +1,98 @@
+<?php
+include "header.php";
+?>
+<style>
+    div.home{
+        height: 1700px;
+    }
+    table, tr, td {
+        border: 1px solid white;
+    }
+    table,tr{
+        display: inline-block;
+        margin-left:auto;
+        margin-right:auto;
+        width: 100%;
+    }
+    tr{
+        text-align: center;
+    }
+    td{
+        margin: 0 10px 20px;
+    }
+    div img{
+        width: 100%;
+        height: 100%;
+    }
+    div{
+        width: 100%;
+        height: 100%;
+        text-align: center;
+    }
+
+
+</style>
+<div class="containers" style="margin-left: 0;">
+    <div class="home">
+        <h1 style="text-align: center;"> TEAM</h1>
+        <hr style="border-top: 1px solid white;">
+        <table>
+            <tbody>
+            <tr align="center">
+                <td valign="center">
+                    <div>
+                        <img src="images/shajeed.png" alt="">
+                        <p>
+                            <span>Mr. Singh</span><br>
+                            President <br>
+                            +1 514 654 3443<br>
+                            abc@gmail.com
+                        </p>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <img src="images/shajeed.png" alt="">
+                        <p>
+                            <span>Mr. Singh</span><br>
+                            President <br>
+                            +1 514 654 3443<br>
+                            abc@gmail.com
+                        </p>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div>
+                        <img src="images/shajeed.png" alt="">
+                        <p>
+                            <span>Mr. Singh</span><br>
+                            President <br>
+                            +1 514 654 3443<br>
+                            abc@gmail.com
+                        </p>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <img src="images/shajeed.png" alt="">
+                        <p>
+                            <span>Mr. Singh</span><br>
+                            President <br>
+                            +1 514 654 3443<br>
+                            abc@gmail.com
+                        </p>
+                    </div>
+                </td>
+
+            </tr>
+
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
