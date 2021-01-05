c7e5ca1b109aa3083342b45f7b6ec56997b41b19
Shajeed <shajeed0@gmail.com>
2021-01-05 01:11:14 -0500

    Updated Team

--- direction.php
+++ direction.php
@@ -2,95 +2,248 @@
 include "header.php";
 ?>
 <style>
-    div.home{
-        height: 1700px;
+    div.home {
+        height: 1300px;
     }
+
     table, tr, td {
         border: 1px solid white;
     }
-    table,tr{
+
+    table, tr {
         display: inline-block;
-        margin-left:auto;
-        margin-right:auto;
+        margin-left: auto;
+        margin-right: auto;
         width: 100%;
     }
-    tr{
+
+    tr {
         text-align: center;
     }
-    td{
+
+    td {
         margin: 0 10px 20px;
     }
-    div img{
+
+    div img {
         width: 100%;
         height: 100%;
     }
-     td div{
+
+    td div {
         width: 100%;
         height: 100%;
         text-align: center;
     }
+
+    /*New Style*/
+
+    html {
+        box-sizing: border-box;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+
+    .column {
+        float: left;
+        width: 33.3%;
+        margin-bottom: 16px;
+        padding: 0 8px;
+    }
+
+    @media screen and (max-width: 650px) {
+        .column {
+            width: 100%;
+            display: block;
+        }
+    }
+
+    .card {
+        /*box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);*/
+        margin: 20px;
+        box-shadow: 5px 5px 10px 0px #FCDBDE;
+        border-radius: 20px;
+        overflow: hidden;
+    }
+
+    .container {
+        background-color: #303133;
+        padding: 0 16px;
+        display: grid;
+    }
+
+    .container::after, .row::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+
+    .title {
+        float: left;
+        color: grey;
+    }
+
+    .button {
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 8px;
+        color: white;
+        background-color: #9E0B17;
+        text-align: center;
+        cursor: pointer;
+        width: 100%;
+        border-radius: 10px;
+    }
+
+    .button:hover {
+        background-color: #F7959D;
+        color: black;
+    }
+
+    h2 {
+        color: white;
+    }
+
 
 
 </style>
 <div class="containers" style="margin-left: 0;">
+    <!--    <div class="home">-->
+    <!--        <h1 style="text-align: center;">TEAM</h1>-->
+    <!--        <hr style="border-top: 1px solid white;">-->
+    <!--        <table>-->
+    <!--            <tbody>-->
+    <!--            <tr align="center">-->
+    <!--                <td valign="center">-->
+    <!--                    <div>-->
+    <!--                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">-->
+    <!--                        <p>-->
+    <!--                            <span>Mr. Singh</span><br>-->
+    <!--                            President <br>-->
+    <!--                            +1 514 654 3443<br>-->
+    <!--                            abc@gmail.com-->
+    <!--                        </p>-->
+    <!--                    </div>-->
+    <!--                </td>-->
+    <!--                <td>-->
+    <!--                    <div>-->
+    <!--                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">-->
+    <!--                        <p>-->
+    <!--                            <span>Mr. Singh</span><br>-->
+    <!--                            President <br>-->
+    <!--                            +1 514 654 3443<br>-->
+    <!--                            abc@gmail.com-->
+    <!--                        </p>-->
+    <!--                    </div>-->
+    <!--                </td>-->
+    <!--            </tr>-->
+    <!--            <tr>-->
+    <!--                <td>-->
+    <!--                    <div>-->
+    <!--                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">-->
+    <!--                        <p>-->
+    <!--                            <span>Mr. Singh</span><br>-->
+    <!--                            President <br>-->
+    <!--                            +1 514 654 3443<br>-->
+    <!--                            abc@gmail.com-->
+    <!--                        </p>-->
+    <!--                    </div>-->
+    <!--                </td>-->
+    <!--                <td>-->
+    <!--                    <div>-->
+    <!--                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">-->
+    <!--                        <p>-->
+    <!--                            <span>Mr. Singh</span><br>-->
+    <!--                            President <br>-->
+    <!--                            +1 514 654 3443<br>-->
+    <!--                            abc@gmail.com-->
+    <!--                        </p>-->
+    <!--                    </div>-->
+    <!--                </td>-->
+    <!---->
+    <!--            </tr>-->
+    <!---->
+    <!---->
+    <!--            </tbody>-->
+    <!--        </table>-->
+    <!--    </div>-->
     <div class="home">
-        <h1 style="text-align: center;">TEAM</h1>
-        <hr style="border-top: 1px solid white;">
-        <table>
-            <tbody>
-            <tr align="center">
-                <td valign="center">
-                    <div>
-                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">
+<!--        1st row --------------------------------------------------------------->
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                         alt="Jane" style="width:100%">
+                    <div class="container">
+                        <h2>Jane Doe</h2>
+                        <p class="title">CEO & Founder</p><br>
+                        <!--                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>-->
+                        <p><a href="mailto:example@example.com">example@example.com</a></p>
                         <p>
-                            <span>Mr. Singh</span><br>
-                            President <br>
-                            +1 514 654 3443<br>
-                            abc@gmail.com
+                            <button class="button">Contact</button>
                         </p>
                     </div>
-                </td>
-                <td>
-                    <div>
-                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">
-                        <p>
-                            <span>Mr. Singh</span><br>
-                            President <br>
-                            +1 514 654 3443<br>
-                            abc@gmail.com
-                        </p>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div>
-                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">
-                        <p>
-                            <span>Mr. Singh</span><br>
-                            President <br>
-                            +1 514 654 3443<br>
-                            abc@gmail.com
-                        </p>
-                    </div>
-                </td>
-                <td>
-                    <div>
-                        <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="">
-                        <p>
-                            <span>Mr. Singh</span><br>
-                            President <br>
-                            +1 514 654 3443<br>
-                            abc@gmail.com
-                        </p>
-                    </div>
-                </td>
+                </div>
+            </div>
 
-            </tr>
+            <div class="column">
+                <div class="card">
+                    <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                         alt="Mike" style="width:100%">
+                    <div class="container">
+                        <h2>Mike Ross</h2>
+                        <p class="title">Art Director</p><br>
+                        <!--                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>-->
+                        <p>example@example.com</p>
+                        <p>
+                            <button class="button">Contact</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                         alt="John" style="width:100%">
+                    <div class="container">
+                        <h2>John Doe</h2>
+                        <p class="title">Designer</p><br>
+                        <!--                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>-->
+                        <p>example@example.com</p>
+                        <p>
+                            <button class="button">Contact</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
 
 
-            </tbody>
-        </table>
+        </div>
+        <!--        2nd row --------------------------------------------------------------->
+
+        <div class="row">
+
+
+            <div class="column">
+                <div class="card">
+                    <img src="https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                         alt="John" style="width:100%">
+                    <div class="container">
+                        <h2>John Doe</h2>
+                        <p class="title">Designer</p><br>
+                        <!--                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>-->
+                        <p>example@example.com</p>
+                        <p>
+                            <button class="button">Contact</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 <?php
